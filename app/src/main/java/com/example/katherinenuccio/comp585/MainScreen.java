a941fd0563f56350d21b8c5d4e4f228e1d1ee5bc
Katie Nuccio <katie.nuccio@hotmail.com>
2017-02-27 17:36:23 -0500

    Restart

--- app/src/main/java/com/example/katherinenuccio/comp585/MainScreen.java
+++ app/src/main/java/com/example/katherinenuccio/comp585/MainScreen.java
@@ -4,11 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainScreen extends AppCompatActivity {
-    public void wall() {
-        if (1 > 0) {
-            System.out.print("katie");
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
