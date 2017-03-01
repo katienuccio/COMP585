6f58ba7b041aed7c2436197b907d98954b784f1b
Katie Nuccio <katie.nuccio@hotmail.com>
2017-03-01 15:00:01 -0500

    Starting tutorial

--- app/src/main/java/com/example/katherinenuccio/comp585/MainScreen.java
+++ app/src/main/java/com/example/katherinenuccio/comp585/MainScreen.java
@@ -3,10 +3,19 @@ package com.example.katherinenuccio.comp585;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.estimote.sdk.SystemRequirementsChecker;
+
 public class MainScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 }
