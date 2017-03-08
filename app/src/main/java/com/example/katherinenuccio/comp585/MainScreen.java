eea5a5236a771062d127195a88fef57554cf3a68
Daniel Pureco <dpureco53@gmail.com>
2017-03-07 19:51:46 -0500

    Helper Screen and Profile Screen. Buttons work now (maybe?). Back navigation to each screen. JOHN DID THIS

--- app/src/main/java/com/example/katherinenuccio/comp585/MainScreen.java
+++ app/src/main/java/com/example/katherinenuccio/comp585/MainScreen.java
@@ -1,15 +1,44 @@
 package com.example.katherinenuccio.comp585;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.estimote.sdk.SystemRequirementsChecker;
 
-public class MainScreen extends AppCompatActivity {
+public class MainScreen extends AppCompatActivity implements View.OnClickListener{
+
+    private Button play;
+    private Button helper;
+    private Button profile;
+
+    private TextView title;
+    private TextView currRoom;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        play = (Button) findViewById(R.id.play_button);
+        play.setOnClickListener(this);
+        helper = (Button) findViewById(R.id.helper_button);
+        helper.setOnClickListener(this);
+        profile = (Button) findViewById(R.id.profile_button);
+        profile.setOnClickListener(this);
+
+        title = (TextView) findViewById(R.id.main_title);
+        currRoom = (TextView) findViewById(R.id.currRoom);
+
+
+
+
     }
 
     @Override
@@ -20,4 +49,17 @@ public class MainScreen extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        if (view == play) {
+            Intent i = new Intent(this, PlayScreen.class);
+            startActivity(i);
+        }else if (view == helper){
+            Intent i = new Intent(this, HelperScreen.class);
+            startActivity(i);
+        }else if (view == profile){
+            Intent i = new Intent(this, ProfileScreen.class);
+            startActivity(i);
+        }
+    }
 }
