a9b6f203f8c723748bdc2126feb735499d7bf761
Daniel Pureco <dpureco53@gmail.com>
2017-04-12 20:59:38 -0400

    Added every room. Added TTS method for ease-of-use and compatibility.

--- app/src/main/java/com/example/katherinenuccio/RoomHunt/BeachScreen.java
+++ app/src/main/java/com/example/katherinenuccio/RoomHunt/BeachScreen.java
@@ -1,8 +1,8 @@
 package com.example.katherinenuccio.RoomHunt;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +10,29 @@ import android.widget.Button;
 import java.util.HashMap;
 
 
-public class ForestScreen extends AppCompatActivity implements View.OnClickListener {
+public class BeachScreen extends AppCompatActivity implements View.OnClickListener {
 
-    private Button forestbutt;
+    private Button beachbutt;
     private HashMap<String, Boolean> flags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forest_screen);
+        setContentView(R.layout.activity_beach_screen);
         Intent intent = getIntent();
         flags = (HashMap<String, Boolean>)intent.getSerializableExtra("flags");
-        flags.put("appleDone", true);
-        forestbutt = (Button) findViewById(R.id.forestbutt);
-        forestbutt.setOnClickListener(this);
+        flags.put("swordDone", true);
+        beachbutt = (Button) findViewById(R.id.beachbutt);
+        beachbutt.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view == forestbutt) {
+        if (view == beachbutt) {
             Intent i = new Intent(this, PlayScreen.class);
             i.putExtra("flags", flags);
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            Log.d("Flags During Forest", flags.toString());
+            Log.d("Flags During Beach", flags.toString());
             startActivity(i);
         }
     }
--- app/src/main/java/com/example/katherinenuccio/RoomHunt/MountainScreen.java
+++ app/src/main/java/com/example/katherinenuccio/RoomHunt/MountainScreen.java
@@ -1,8 +1,8 @@
 package com.example.katherinenuccio.RoomHunt;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +10,29 @@ import android.widget.Button;
 import java.util.HashMap;
 
 
-public class ForestScreen extends AppCompatActivity implements View.OnClickListener {
+public class MountainScreen extends AppCompatActivity implements View.OnClickListener {
 
-    private Button forestbutt;
+    private Button mountainbutt;
     private HashMap<String, Boolean> flags;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forest_screen);
+        setContentView(R.layout.activity_mountain_screen);
         Intent intent = getIntent();
         flags = (HashMap<String, Boolean>)intent.getSerializableExtra("flags");
-        flags.put("appleDone", true);
-        forestbutt = (Button) findViewById(R.id.forestbutt);
-        forestbutt.setOnClickListener(this);
+        flags.put("dragonDone", true);
+        mountainbutt = (Button) findViewById(R.id.mountainbutt);
+        mountainbutt.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if (view == forestbutt) {
+        if (view == mountainbutt) {
             Intent i = new Intent(this, PlayScreen.class);
             i.putExtra("flags", flags);
             i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            Log.d("Flags During Forest", flags.toString());
+            Log.d("Flags During Mountain", flags.toString());
             startActivity(i);
         }
     }
