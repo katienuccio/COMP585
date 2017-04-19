85d0ac7fcb7b92dbb66407bc46704c831bc1ecc3
katienuccio <katie.nuccio@hotmail.com>
2017-04-19 16:49:01 -0400

    finished mountain screen wooooo!

--- app/src/main/java/com/example/katherinenuccio/RoomHunt/MountainScreen.java
+++ app/src/main/java/com/example/katherinenuccio/RoomHunt/MountainScreen.java
@@ -1,82 +1,197 @@
 package com.example.katherinenuccio.RoomHunt;
-
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Locale;
 
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
-public class MountainScreen extends AppCompatActivity implements View.OnClickListener {
+public class MountainScreen extends Activity implements RecognitionListener {
 
+    private TextView returnedText;
+    private ToggleButton toggleButton;
+    private ProgressBar progressBar;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
+    private String LOG_TAG = "VoiceRecognition";
+    private String mResult;
     private Button mountainbutt;
     private HashMap<String, Boolean> flags;
     private TextView resultTEXT;
     private TextView newText;
+    private boolean listening;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mountain_screen);
-        resultTEXT = (TextView) findViewById(R.id.TVresult);
-        newText = (TextView) findViewById(R.id.result);
+        returnedText = (TextView) findViewById(R.id.textView1);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+//        toggleButton = (ToggleButton) findViewById(R.id.toggleButton1);
         Intent intent = getIntent();
         flags = (HashMap<String, Boolean>)intent.getSerializableExtra("flags");
         flags.put("dragonDone", true);
-        mountainbutt = (Button) findViewById(R.id.mountainbutt);
-        mountainbutt.setOnClickListener(this);
-    }
+        progressBar.setVisibility(View.INVISIBLE);
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(this);
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS,true);
+        listening = false;
 
-    public void onButtonClick(View v) {
-        if (v.getId() == R.id.imageButton) {
-            promptSpeechInput();
-        }
-    }
+        ConstraintLayout rlayout = (ConstraintLayout) findViewById(R.id.mountainlayout);
+        rlayout.setOnClickListener(new View.OnClickListener() {
 
-    public void promptSpeechInput() {
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
-
-        try {
-            startActivityForResult(i, 100);
-        } catch (ActivityNotFoundException e) {
-            Toast.makeText(com.example.katherinenuccio.RoomHunt.MountainScreen.this, "Sorry! Your device doesn't support speech language", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void onActivityResult(int request_code, int result_code, Intent i) {
-        super.onActivityResult(request_code, result_code, i);
-        switch (request_code) {
-            case 100: if (result_code == RESULT_OK && i != null) {
-                ArrayList<String> result = i.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                resultTEXT.setText("You said: " + result.get(0));
-                if (result.get(0).toLowerCase().equals("i have the power")){
-                    beatBoss();
-                }
-                else {
-                    newText.setText("Sorry, that's not the right phrase");
+            @Override
+            public void onClick(View v) {
+                if (!listening) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    progressBar.setIndeterminate(true);
+                    speech.startListening(recognizerIntent);
+                    listening = true;
+                } else {
+                    progressBar.setIndeterminate(false);
+                    progressBar.setVisibility(View.INVISIBLE);
+                    speech.stopListening();
+                    listening = false;
                 }
             }
-                break;
+
+        });
+
+//        toggleButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+//
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView,
+//                                         boolean isChecked) {
+//                if (isChecked) {
+//                    progressBar.setVisibility(View.VISIBLE);
+//                    progressBar.setIndeterminate(true);
+//                    speech.startListening(recognizerIntent);
+//                } else {
+//                    progressBar.setIndeterminate(false);
+//                    progressBar.setVisibility(View.INVISIBLE);
+//                    speech.stopListening();
+//                }
+//            }
+//        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (speech != null) {
+            speech.destroy();
+            Log.i(LOG_TAG, "destroy");
+        }
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.i(LOG_TAG, "onBeginningOfSpeech");
+        progressBar.setIndeterminate(false);
+        progressBar.setMax(10);
+        listening = true;
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+        Log.i(LOG_TAG, "onBufferReceived: " + buffer);
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i(LOG_TAG, "onEndOfSpeech");
+//        progressBar.setIndeterminate(true);
+        progressBar.setIndeterminate(false);
+        progressBar.setVisibility(View.INVISIBLE);
+        speech.stopListening();
+        listening = false;
+
+//        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onError(int errorCode) {
+        returnedText.setText(mResult);
+        if (mResult.toLowerCase().equals("i have the power")){
+            beatBoss();
+        }
+        else {
+            returnedText.setText("Sorry, that's not the right phrase");
+        }
+//        toggleButton.setChecked(false);
+
+//        String errorMessage = getErrorText(errorCode);
+//        Log.d(LOG_TAG, "FAILED " + errorMessage);
+//        returnedText.setText(errorMessage);
+//        toggleButton.setChecked(false);
+    }
+
+    @Override
+    public void onEvent(int arg0, Bundle arg1) {
+        Log.i(LOG_TAG, "onEvent");
+    }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+        ArrayList<String> data = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        ArrayList<String> unstableData = partialResults.getStringArrayList("android.speech.extra.UNSTABLE_TEXT");
+        mResult = data.get(0) + unstableData.get(0);
+        Log.i(LOG_TAG, "onPartialResults");
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {
+        Log.i(LOG_TAG, "onReadyForSpeech");
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i(LOG_TAG, "onResults");
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+        String text = "";
+        for (String result : matches)
+            text += result + "\n";
+
+        returnedText.setText(text);
+        if (text.toLowerCase().equals("i have the power")){
+            beatBoss();
+        }
+        else {
+            returnedText.setText("Sorry, that's not the right phrase");
         }
     }
 
     @Override
-    public void onClick(View view) {
-        if (view == mountainbutt) {
-            beatBoss();
-        }
+    public void onRmsChanged(float rmsdB) {
+        Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
+        progressBar.setProgress((int) rmsdB);
     }
 
     public void beatBoss() {
@@ -86,4 +201,42 @@ public class MountainScreen extends AppCompatActivity implements View.OnClickLis
         Log.d("Flags During Mountain", flags.toString());
         startActivity(i);
     }
+
+    public static String getErrorText(int errorCode) {
+        String message;
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "Audio recording error";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "Client side error";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "Insufficient permissions";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "No match";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "error from server";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "No speech input";
+                break;
+            default:
+                message = "Didn't understand, please try again.";
+                break;
+        }
+        return message;
+    }
+
 }
