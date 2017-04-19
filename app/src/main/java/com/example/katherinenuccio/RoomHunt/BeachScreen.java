c66d4f0cf3d4874a0897ad2ebc9c30c9e121b2d0
Daniel <dpureco53@gmail.com>
2017-04-19 16:46:25 -0400

    Added TTS in beach

--- app/src/main/java/com/example/katherinenuccio/RoomHunt/BeachScreen.java
+++ app/src/main/java/com/example/katherinenuccio/RoomHunt/BeachScreen.java
@@ -2,10 +2,12 @@ package com.example.katherinenuccio.RoomHunt;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.speech.tts.TextToSpeech;
 import android.widget.Button;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
@@ -14,6 +16,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.HashMap;
+import java.util.Locale;
 
 
 public class BeachScreen extends AppCompatActivity implements View.OnClickListener {
@@ -21,6 +24,7 @@ public class BeachScreen extends AppCompatActivity implements View.OnClickListen
     private Button beachbutt;
     private HashMap<String, Boolean> flags;
     private TextView shakes;
+    private TextToSpeech tts;
     // The following are used for the shake detection
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
@@ -36,6 +40,23 @@ public class BeachScreen extends AppCompatActivity implements View.OnClickListen
         flags = (HashMap<String, Boolean>)intent.getSerializableExtra("flags");
         flags.put("swordDone", true);
         Log.d("Beach", "Made it");
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                Log.d("Test", "Init");
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = tts.setLanguage(Locale.US);
+                    Log.e("TTS", "Initialization Succeeded");
+                    speak("Welcome to the Beach. Shake your phone to find the sword of legends.");
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "This Language Is Not Supported");
+                    }
+                } else {
+                    Log.e("TTS", "Initialization Failed");
+                }
+            }
+        });
+
         beachbutt = (Button) findViewById(R.id.beachbutt);
         beachbutt.setOnClickListener(this);
         // ShakeDetector initialization
@@ -60,9 +81,12 @@ public class BeachScreen extends AppCompatActivity implements View.OnClickListen
 
     private void handleShakeEvent(int count) {
         if(totalShakes >= 10) {
+            speak("You did it! Return to town.");
             swordDone();
         } else {
             totalShakes++;
+            if(totalShakes == 3){speak("You see something in the sand! Keep digging.");}
+            if(totalShakes == 8){speak("You're almost there!");}
             Log.d("Beach", "Shooketh " + totalShakes);
             shakes.setText("Total: " + totalShakes);
         }
@@ -88,6 +112,15 @@ public class BeachScreen extends AppCompatActivity implements View.OnClickListen
         // Add the following line to unregister the Sensor Manager onPause
         mSensorManager.unregisterListener(mShakeDetector);
         super.onPause();
+    }
+
+    // Text to speech code. For deprecation/compatibility purposes.
+    private void speak(String text) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
     }
 
     @Override
